27dbc5c70c137b9e4b9a1c58f7ff076d1f628cdb
t-anano <ananor@aggies.ncat.edu>
2023-10-18 10:22:36 -0400

    First program for DS&A, Goodrich et. al

---
+++ firstprogram.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib> 
+#include <iostream> 
+
+int main(){
+
+std::cout << "Please enter two numbers: " << std::endl; 
+int x , y; 
+std::cin >> x >> y; 
+int sum = x+y; 
+std::cout << "Their sum is " << sum << std::endl; 
+    return 0; 
+}
