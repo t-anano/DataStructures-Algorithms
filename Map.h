6e003420459b7089b16d76bb74341e0ef1c28ac6
t-anano <ananor@aggies.ncat.edu>
2023-10-20 10:33:10 -0400

    Map and Tree Interfaces

---
+++ Map.h
@@ -0,0 +1,15 @@
+template <typename K, typename V>
+class Map { // map interface
+public:
+class Entry; // a (key,value) pair
+class Iterator; // an iterator (and position)
+int size() const; // number of entries in the map
+bool empty() const; // is the map empty?
+Iterator find(const K& k) const; // find entry with key k
+Iterator put(const K& k, const V& v); // insert/replace pair (k,v)
+void erase(const K& k) // remove entry with key k
+throw(NonexistentElement);
+void erase(const Iterator& p); // erase entry at p
+Iterator begin(); // iterator to first entry
+Iterator end(); // iterator to end entry
+};
